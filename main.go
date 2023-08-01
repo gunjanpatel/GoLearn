02816e06e0694b8a576daac5371ea0f071add4ed
Gunjan Patel <guapa@vestas.com>
2023-08-01 17:43:51 +0200

    Declaring variables

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello != World!")
+	// Ways to declare variables
+	// var firstName, lastName string
+	// var (
+	// 	firstName, lastName string
+	// 	age int
+	// )
+	// var (
+	// 	firstName = "Gunjan"
+	// 	// firstName string = "Gunjan"
+	// 	lastName  = "Patel"
+	// 	//lastName string = "Patel"
+	// 	age = 32
+	// 	//age int = 32
+	// )
+	firstName, lastName := "Gunjan", "Patel"
+	age := 32
+	fmt.Printf("%s %s is %d years old", firstName, lastName, age)
 }
