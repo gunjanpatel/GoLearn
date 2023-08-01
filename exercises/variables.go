0df7b710cb669d3f89ff6d1960cba826587a3a8a
Gunjan Patel <guapa@vestas.com>
2023-08-01 18:22:57 +0200

    Type conversion

--- exercises/variables.go
+++ exercises/variables.go
@@ -19,5 +19,14 @@ func main() {
 	// )
 	firstName, lastName := "Gunjan", "Patel"
 	age := 32
-	fmt.Printf("%s %s is %d years old", firstName, lastName, age)
+	fmt.Printf("%s %s is %d years old\n", firstName, lastName, age)
+
+	// Checking default values
+	var defaultInt int
+	var defaultFloat32 float32
+	var defaultFloat64 float64
+	var defaultBool bool
+	var defaultString string
+	fmt.Println("Following are the default values of each data types:")
+	fmt.Println(defaultInt, defaultFloat32, defaultFloat64, defaultBool, defaultString)
 }
