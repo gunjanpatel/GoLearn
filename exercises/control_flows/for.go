e83c2a932bb12ec1a0fdd2d0e5707edbb4c3c38f
Gunjan Patel <guapa@vestas.com>
2023-08-02 11:52:33 +0200

    More fun with for

--- exercises/control_flows/for.go
+++ exercises/control_flows/for.go
@@ -21,6 +21,17 @@ func infiniteBreak() {
 	}
 }
 
+func sumOfExcludingMod5Numbers() {
+	sum := 0
+	for num := 1; num <= 100; num++ {
+		if num%5 == 0 {
+			continue
+		}
+		sum += num
+	}
+	fmt.Println("The sum of 1 to 100, but excluding numbers divisible by 5, is", sum)
+}
+
 func main() {
 	var num int64
 	// Deprecated
