7cabf0f42533d408b1162b12707c8e87727def9a
Gunjan Patel <guapa@vestas.com>
2023-08-03 00:58:31 +0200

    Update fibonacci

--- exercises/data_structure/fibonacci.go
+++ exercises/data_structure/fibonacci.go
@@ -27,3 +27,18 @@ func fibonacci(number int) []int {
 
 	return output
 }
+
+func fibonacci2(n int) []int {
+	if n < 2 {
+		return make([]int, 0)
+	}
+
+	nums := make([]int, n)
+	nums[0], nums[1] = 1, 1
+
+	for i := 2; i < n; i++ {
+		nums[i] = nums[i-1] + nums[i-2]
+	}
+
+	return nums
+}
