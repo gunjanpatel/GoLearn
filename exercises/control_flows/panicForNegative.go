50418a1c5d20fc9c5828f826fd2445d5e2acbdf4
Gunjan Patel <guapa@vestas.com>
2023-08-02 17:14:50 +0200

    Using handler

--- exercises/control_flows/panicForNegative.go
+++ exercises/control_flows/panicForNegative.go
@@ -6,8 +6,32 @@ import (
 
 func main() {
 	val := 0
+	defer func() {
+		handler := recover()
+
+		if handler != nil {
+			fmt.Println(handler)
+		}
+	}()
+
 	for {
 		fmt.Print("Enter number: ")
+		fmt.Scanf("%d", &val)
+
+		switch {
+		case val < 0:
+			panic("You entered a negative number!")
+		case val == 0:
+			fmt.Println("0 is neither negative nor positive")
+		default:
+			fmt.Println("You entered:", val)
+		}
+	}
+}
+
+func find(val int) bool {
+	for {
+		fmt.Println("Enter number: ")
 
 		fmt.Scanf("%d", &val)
 
