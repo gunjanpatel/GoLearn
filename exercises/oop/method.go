dd9342e58d948eb3035697836c5dde913613d912
Gunjan Patel <guapa@vestas.com>
2023-08-07 23:06:05 +0200

    Pointers

--- exercises/oop/method.go
+++ exercises/oop/method.go
@@ -14,6 +14,10 @@ func (t triangel) perimeter() int {
 	return t.size * 3
 }
 
+func (t *triangel) doubleSize() {
+	t.size *= 2
+}
+
 func (s square) perimeter() int {
 	return s.size * 4
 }
@@ -22,6 +26,10 @@ func main() {
 	t := triangel{size: 3}
 	s := square{size: 4}
 
+	fmt.Println("triagle size", t)
 	fmt.Println("Perimeter: triagle", t.perimeter())
+
+	t.doubleSize()
+	fmt.Println("Perimeter: triangle after double size", t.perimeter())
 	fmt.Println("Perimeter: sqare", s.perimeter())
 }
